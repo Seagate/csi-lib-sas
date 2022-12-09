3f6530705968c4cd9af33230f13db31bc09886f7
David White <david.white@seagate.com>
2022-12-09 11:38:28 -0700

    refactor: refactor device discovery process
    Change logic in discoverDevices to increase chances of discovering
    multipath devices properly by retrying discovery multiple times if only
    non-multipath device is found. Refactor FindDiskById (and make it public)
    to not read the entire disk/by-id directory during device search

--- sas/sas_test.go
+++ sas/sas_test.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-   http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -102,7 +102,7 @@ func TestSearchDisk(t *testing.T) {
 func TestInvalidWWN(t *testing.T) {
 	logger, _ := ktesting.NewTestContext(t)
 	testWwn := "INVALIDWWN"
-	dm, devices := findDiskById(logger, testWwn, &fakeIOHandler{})
+	dm, devices := FindDiskById(logger, testWwn, &fakeIOHandler{})
 
 	if len(devices) > 0 || dm != "" {
 		t.Error("Found a disk with WWN that does not Exist")
